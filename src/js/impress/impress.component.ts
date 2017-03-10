15b03f077806080a3838cefb6b72ee7897eccd7a
Rey Ramos <ramor11@labcorp.com>
2017-03-10 08:49:22 -0500

    bug fix

--- src/js/impress/impress.component.ts
+++ src/js/impress/impress.component.ts
@@ -24,10 +24,6 @@ class ImpressComponentCtrl {
     }
 
     $onInit() {
-        if ("ontouchstart" in document.documentElement) {
-            document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
-        }
-
         impress().init();
     }
 
